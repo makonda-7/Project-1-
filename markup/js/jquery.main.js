757126a36c1a098a2f6cba7d102f9b00a82b6b38
makonda-7 <makonda-7@mail.ru>
2018-06-19 13:41:47 +0300

    Initial commit

---
+++ markup/js/jquery.main.js
@@ -0,0 +1,18 @@
+jQuery(function(){
+	initMenu();
+	initOpenClose();
+});
+
+function initMenu() {
+	var opener = jQuery('.menu-open');
+	var page = jQuery('html');
+	var classActive = 'menu-active';
+
+	var clickHandler = function(e) {
+		e.preventDefault();
+		page.toggleClass(classActive);
+	};
+	opener.on('click', clickHandler);
+}
+
+
